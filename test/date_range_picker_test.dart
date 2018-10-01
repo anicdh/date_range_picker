f2506ea41530b8155bc31c5ca7b266ae31f94678
andh <anic.dh@gmail.com>
2018-10-01 23:28:28 +0700

    config for release on pub package

--- test/date_range_picker_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-
-import 'package:date_range_picker/date_range_picker.dart';
-
-void main() {
-  test('will write test later', () {
-    expect(true, true);
-  });
-}
